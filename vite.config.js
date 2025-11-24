7902aa12e812faac8775892dadfe7914a19c47fc
소부승 <68296100+bootkorea@users.noreply.github.com>
2025-11-24 13:46:02 +0900

    edit root url

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/bootkorea.github.io/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // For username.github.io repositories, base should be '/'
 })
